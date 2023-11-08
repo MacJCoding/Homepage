07fc3444dbfe2a751733496574ab4d6dfeac2e99
MacJCoding <mjcoding99@gmail.com>
2023-11-08 17:29:43 -0500

    Mobile Button Fix Part 4

--- script.js
+++ script.js
@@ -78,13 +78,11 @@ req.onreadystatechange = () => {
 };*/
 
 const randomButton = document.querySelector("#randomButton");
-randomButton.addEventListener("click", newVideo);
-
-function newVideo(){
+randomButton.addEventListener("click", () => { 
 	var a = Math.floor(Math.random() * videoList.length);
 	randomVidContainer.innerHTML = "<h3>"+videoList[a]['name']+"</h3>"+//<a style='text-decoration: none; color:black' href='https://youtu.be/"+videoList[a]['link']+"'>Watch Here</a></br>"+
 	'<iframe width="560" height="560" src="https://www.youtube.com/embed/'+videoList[a]['link']+'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
-}
+});
 /*
 req.open("GET", "https://api.jsonbin.io/v3/b/654a7abb12a5d376599630e6/latest", true);
 req.setRequestHeader("X-Access-Key", "$2a$10$7V8rdmDR5yINKSL07ixq/ORz2fYnIGHnv9C/dFK1/hJY/0eCvL/VW");
